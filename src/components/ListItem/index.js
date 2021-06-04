fc1b96aeb416092e734dc448770247bc415d39fe
Monica261 <desenvolvimento.midiasim@gmail.com>
2021-06-04 14:31:42 -0300

    persistindo links

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {Feather} from '@expo/vector-icons';
 
-import {} from './styles';
+import {ContainerButton, Item} from './styles';
 
 export default function ListItem(){
     return(
         <View>
-            <Text>Link</Text>
+            <ContainerButton activeOpacity={0.9} onPress={() => alert('test')}>
+                <Feather name="link" color="#fff" size={24}/>
+                <Item numberOfLines={1}>https://www.nasa.gov/</Item>
+            </ContainerButton>
         </View>
     )
 }
